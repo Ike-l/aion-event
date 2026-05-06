b39f3cf831348be636ffe24c61a378534d5015f6
isaac <isaacloff@gmail.com>
2026-05-06 08:54:26 +0100

    event system

---
+++ src/event_system/mod.rs
@@ -0,0 +1,9 @@
+use std::sync::Arc;
+
+use aion_program::prelude::ProgramRegistry;
+
+use crate::prelude::EventBuffer;
+
+pub trait EventSystem {
+    fn execute(program_registry: &Arc<ProgramRegistry>, current_events: &EventBuffer) -> EventBuffer;
+}
