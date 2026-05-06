ce8e07d245bbdea79b6355c00a5d791ccd5a7aab
isaac <isaacloff@gmail.com>
2026-05-06 08:54:19 +0100

    event buffer

---
+++ src/event_buffer/mod.rs
@@ -0,0 +1,20 @@
+use crate::prelude::Event;
+
+#[derive(Debug, Default)]
+pub struct EventBuffer {
+    events: Vec<Event>
+}
+
+impl EventBuffer {
+    pub fn read(&self) -> impl Iterator<Item = &Event> {
+        self.events.iter()
+    }
+
+    pub fn new(events: Vec<Event>) -> Self {
+        Self { events }
+    }
+
+    pub fn merge(&mut self, other: impl Iterator<Item = Event>) {
+        self.events.extend(other);
+    }
+}
