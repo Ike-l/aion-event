1b5141f1f6413663a89a3ab1ade85541196dabbe
isaac <isaacloff@gmail.com>
2026-05-06 15:25:16 +0100

    event buffer contains hash set

--- src/event_buffer/mod.rs
+++ src/event_buffer/mod.rs
@@ -1,8 +1,10 @@
+use std::collections::HashSet;
+
 use crate::prelude::Event;
 
 #[derive(Debug, Default)]
 pub struct EventBuffer {
-    events: Vec<Event>
+    events: HashSet<Event>
 }
 
 impl EventBuffer {
@@ -10,12 +12,16 @@ impl EventBuffer {
         self.events.iter()
     }
 
-    pub fn new(events: Vec<Event>) -> Self {
-        Self { events }
+    pub fn new(events: impl Iterator<Item = Event>) -> Self {
+        Self { events: events.collect() }
     }
 
     pub fn extend(&mut self, other: impl Iterator<Item = Event>) {
         self.events.extend(other);
+    }
+
+    pub fn insert(&mut self, event: Event) {
+        self.events.insert(event);
     }
 
     pub fn contains(&self, event: &Event) -> bool {
